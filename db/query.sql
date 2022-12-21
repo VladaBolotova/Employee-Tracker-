39c32acb9f0ffbacb66b13f25ab0a7682adf23f7
VladaBolotova <vladislavabolotova@gmail.com>
2022-12-20 21:31:48 -0500

    finishing query sql

--- db/query.sql
+++ db/query.sql
@@ -1,2 +1,6 @@
 USE  employee_db;
 SELECT role.id, role.title, role.salary, department.name FROM role LEFT JOIN department ON role.department_id = department.id;
+
+USE  employee_db;
+SELECT employee.id, employee.first_name, employee.last_name, role.title , role.salary, department.name FROM employee LEFT JOIN role ON employee.role_id = role.id 
+JOIN department ON role.department_id = department.id;
